08c44295b81e7d388a7c8fdbe8418269159d8b20
Андрій <64520200+Andrewsabatyuk@users.noreply.github.com>
2020-10-23 15:18:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let bodyElement = document.body;
+let cardElement = document.createElement('div');
+let imageContainer = document.createElement('div');
+let infoContainer = document.createElement('div');
+let imageElement = document.createElement('img');
+let headingElement = document.createElement('h5');
+let paragraphElement = document.createElement('p');
+let btnElement = document.createElement('a');
+
+cardElement.className = 'card';
+imageContainer.className = 'image-container';
+infoContainer.className = 'info-container';
+imageElement.className = 'image';
+headingElement.className = 'heading';
+paragraphElement.className = 'paragraph';
+btnElement.className = 'btn';
